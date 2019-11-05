cd0b674247744b23cdcd6360c3c68e9e35dfbbe9
muratvuranok <muratvuranok@hotmail.com>
2019-11-05 13:58:11 +0300

    Odevler

---
+++ Donguler/Form6.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Donguler
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOrnekBir_Click(object sender, EventArgs e)
+        {
+            // Yan yana 5 adet X yazdırınız
+        }
+
+        private void btnOrnekIki_Click(object sender, EventArgs e)
+        {
+            // Alt alta 5 adet X yazdırınız
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Verilen değere göre X ile kare çizdiriniz
+            /*
+             X X X X X X X X X X
+             X X X X X X X X X X
+             X X X X X X X X X X
+             X X X X X X X X X X
+             X X X X X X X X X X
+             X X X X X X X X X X
+             X X X X X X X X X X 
+             
+             
+             */
+        }
+
+        private void btnOrnekDort_Click(object sender, EventArgs e)
+        {
+            // X iler dik kenar üçgen yazdırınız
+            /*
+  
+ 
+             X 
+             X X
+             X X X
+             X X X X
+             X X X X X
+             X X X X X X
+             X X X X X X X
+             
+             
+             */
+        }
+
+        private void btnOrnekBes_Click(object sender, EventArgs e)
+        {
+            // Listbox'a çarpım tablosonu ekleyiniz 1 ile 10(dahil) arası
+
+            //    1 X 1  = 1 
+            //    1 X 2  = 2 
+            //    1 X 3  = 3 
+            //    1 X 4  = 4 
+            //    1 X 5  = 5 
+            //    1 X 6  = 6 
+            //    1 X 7  = 7 
+            //    1 X 8  = 8 
+            //    1 X 9  = 9 
+            //    1 X 10 = 10
+            //    ----------------------
+            //    2 X 1  = 2 
+            //    2 X 2  = 4 
+            //    2 X 3  = 6 
+            //    2 X 4  = 8 
+            //    2 X 5  = 10 
+            //    2 X 6  = 12
+            //    2 X 7  = 14
+            //    2 X 8  = 16
+            //    2 X 9  = 18
+            //    2 X 10 = 20
+            //    ----------------------
+
+        }
+    }
+}
